f7656077ec9aba0d2d7bc26324d32c23dfdff917
Patrick <patrick.guarnieri@hotmail.com>
2021-11-22 20:57:12 +0100

    First Commint

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics, serializers
+from .serializers import RoomSerializer
+from .models import Room
+
+# Create your views here.
+
+class RoomView(generics.ListAPIView): # Allow us to view and create a Room.
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
